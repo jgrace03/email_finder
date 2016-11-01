727326d416fd49e941b7c25770818aa65282cefc
jgrace03 <jusugrace@gmail.com>
2016-11-01 01:06:53 -0400

    simple updates

--- email_finder.py
+++ email_finder.py
@@ -10,24 +10,28 @@ import sys
 import requests  
 from BeautifulSoup import BeautifulSoup
 import re
+import urllib2
 
 website = sys.argv[1]
+
 q = []
-q.append("http://www.%s" %(website))
+
+if "http://" not in website:
+	q.append("http://%s" %(website))
+else:
+	q.append(website)
+
+req = urllib2.Request(q[0])
+res = urllib2.urlopen(req)
+finalurl = res.geturl()
+
+domain = finalurl.split("//")[-1].split("/")[0]
+
 alllinks = []
 alllinks.append(q[0])
 emails = []
 
 while len(q) > 0:
-
-	#check that address is valid
-	while "//" not in q[0]:
-		q.remove(q[0])
-		if len(q) == 0:
-			break
-
-	if len(q) == 0:
-		break
 
 	responce = requests.get(q[0])
 	html = responce.content
@@ -37,9 +41,10 @@ while len(q) > 0:
 	#scrape for other web pages
 	for link in soup.findAll('a'):
 		link = link.get('href')
-		
+				
 		if link:
-			if website in link and '%' not in link and 'pdf' not in link:
+			if (link.split("//www.")[-1].split("/")[0] == domain
+				or link.split("//")[-1].split("/")[0] == domain):
 				if link[0] != 'h':
 					link = "http:%s" %(link)
 				if link not in alllinks:
@@ -50,7 +55,9 @@ while len(q) > 0:
 	match = re.findall(r'[\w\.-]+@[\w\.-]+', html)
 
 	for m in match:
-		if m not in emails and m[len(m)-1] != '.' and '.' in m:
+		if m not in emails and '.' in m:
+			if m[len(m)-1] == '.':
+				m = m[:-1]
 			emails.append(m)
 			print m
 
